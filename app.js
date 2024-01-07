38fab0d483be3b0caa387f5c3364029ee3dd2671
Riyaz Ahmed <24riyazahmedaug2004@gmail.com>
2024-01-07 20:28:39 +0530

    Added JavaScript & updated CSS

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 let weather = {
-      "apiKey":"7a59ab5be1b61d171a09959f289c7e38"
-}
+  apiKey: "7a59ab5be1b61d171a09959f289c7e38",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, speed, temp, humidity);
+    document.querySelector(".city").innerText = name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + " °C";
+    document.querySelector(".humidity").innerText =
+      "Humidity : " + humidity + "%";
+    document.querySelector(".wind-speed").innerText =
+      "Wind Speed : " + speed + "Km/Hr";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector("#search-bar").value);
+  },
+};
+
+document.querySelector(".search-btn").addEventListener("click", function () {
+  weather.search();
+});
